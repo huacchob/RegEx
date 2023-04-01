8e0248353f00d10779de9cbd2819ec23d001e57f
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2023-04-01 16:26:25 -0400

    Changed Python file

--- Config finder/re_config_file.py
+++ Config finder/re_config_file.py
@@ -1,17 +1,14 @@
 import re
 import json
-import os
 
 expression = r'interf[\w\d ]*\s*desc[\w -]*\s*ip[a-z ]*100[\d\. ]*\s*[\w -]*\s*[\w\. ]*'
-folder = r"C:\Users\huacc\OneDrive\Documents\config\\"
+folder = r"C:\Users\huacc\OneDrive\Documents\config\bakcahe-bsr1.config.log.txt"
 
-for filename in os.listdir(folder):
-    file = open(os.path.join(folder, filename), "r", encoding="utf-8")
-    contents = file.read()
-    result = re.findall(expression, contents, flags=re.IGNORECASE)
-    file.close()
-    pretty = json.dumps(result, indent=4)
-    output_filename = os.path.splitext(filename)[0] + '.txt'
-    output_path = os.path.join(folder, output_filename)
-    with open(output_path, 'w', encoding='utf-8') as output_file:
-        output_file.write(str(pretty))
+
+file = open(r"C:\Users\huacc\OneDrive\Documents\config\bakcahe-bsr1.config.log.txt", 'r', encoding='utf-8')
+contents = file.read()
+result = re.findall(expression, contents, flags=re.IGNORECASE)
+file.close()
+pretty = json.dumps(result, indent=4)
+mod_file = open(r"C:\Users\huacc\OneDrive\Documents\config\mf.txt", 'w', encoding='utf-8')
+mod_file.write(pretty)
