8e51dfac3266a1cdf529a5350fdf00cd3bc1c486
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2023-04-01 16:50:14 -0400

    changes

--- Config finder/re_config_file.py
+++ Config finder/re_config_file.py
@@ -2,14 +2,14 @@ import re
 import json
 
 expression = r'interf[\w\d ]*\s*desc[\w -]*\s*ip[a-z ]*100[\d\. ]*\s*[\w -]*\s*[\w\. ]*'
-folder = r"C:\Users\huacc\OneDrive\Documents\config\bakcahe-bsr1.config.log.txt"
-
 
 file = open(r"C:\Users\huacc\OneDrive\Documents\config\bakcahe-bsr1.config.log.txt", 'r', encoding='utf-8')
 contents = file.read()
 result = re.findall(expression, contents, flags=re.IGNORECASE)
 file.close()
+
 pretty = json.dumps(result, indent=4)
+
 mod_file = open(r"C:\Users\huacc\OneDrive\Documents\config\mf.txt", 'w', encoding='utf-8')
 mod_file.write(pretty)
 mod_file.close()
